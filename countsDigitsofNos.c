dfbeb1cd4ed281db9df2310ed29796e82c8e485b
saanvi6306 <25113cn061@glbitm.ac.in>
2026-05-28 23:24:58 +0530

    Day1

---
+++ countsDigitsofNos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int num ,count = 0;
+    printf("Enter a number :");
+    scanf("%d" ,&num);
+
+    while(num != 0){
+        num = num / 10;
+        count ++;
+    }
+    printf("number of digits = %d" ,count);
+    return 0;
+}
