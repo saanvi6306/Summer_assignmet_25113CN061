86f902d6b2968c33c315997ce72172f299322055
saanvi6306 <25113cn061@glbitm.ac.in>
2026-06-02 01:25:57 +0530

    Day2 assignment is uploaded

---
+++ Day2/SumofDigitsofNumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int n,sum=0,d;
+    printf("Enter a number :");
+    scanf("%d",&n);
+
+    while(n !=0){
+        d = n%10;
+        sum += d;
+        n=n/10;
+    }
+    printf("Sum of digits =%d\n",sum);
+    return 0;
+}
