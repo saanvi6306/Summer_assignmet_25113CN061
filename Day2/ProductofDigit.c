645129826d854b2e2cc7a961e9b28aaa8a278920
saanvi6306 <25113cn061@glbitm.ac.in>
2026-06-02 16:11:40 +0530

    Day2 assignment is uploaded

---
+++ Day2/ProductofDigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n,d,product =1;
+    printf("Enter a number :");
+    scanf("%d",&n);
+
+    while(n !=0){
+        d= n%10;
+        product *= d;
+        n = n/10;
+    }
+    printf("product of digits =%d\n",product);
+    return 0;
+
+}
