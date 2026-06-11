9d564ff8cf8b46a6e68c9e6be1ac0b78fa37cda8
saanvi6306 <25113cn061@glbitm.ac.in>
2026-06-11 14:05:21 +0530

    Day11 assignment is uploaded

---
+++ Day11/SumOfTwoNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// using function
+int sum(int a, int b) {
+    return a + b;
+}
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    
+    int result = sum(num1, num2);
+    printf("The sum of %d and %d is: %d\n", num1, num2, result);
+    
+    return 0;
+}
