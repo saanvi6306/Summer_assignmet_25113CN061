5d26974ac777938f6320c9c5a59386a3a92d6ae8
saanvi6306 <25113cn061@glbitm.ac.in>
2026-06-20 12:37:17 +0530

    Day20 assignment is uploaded

---
+++ Day20/CheckSymmetricMatrix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j, flag = 1;
+
+    printf("Enter the order of the matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if(flag)
+        printf("The matrix is Symmetric.\n");
+    else
+        printf("The matrix is Not Symmetric.\n");
+
+    return 0;
+}
