52af887412b6c59c87912b7ab868058142bcca3b
saanvi6306 <25113cn061@glbitm.ac.in>
2026-06-25 12:01:38 +0530

    Day25 assignment is uploaded

---
+++ Day25/SortNamesAlphabeti.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char names[10][50];
+    int n, i, j;
+    char temp[50];
+
+    printf("Enter the number of names: ");
+    scanf("%d", &n);
+
+    printf("Enter the names: ");
+    for(i=0; i<n; i++){
+        scanf("%s", names[i]);
+    }
+
+    for(i=0; i<n-1; i++){
+        for(j=i+1; j<n; j++){
+            if(strcmp(names[i], names[j])>0){
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+
+    printf("Names in alphabetical order are: ");
+    for(i=0; i<n; i++){
+        printf("%s ", names[i]);
+    }
+    return 0;
+}
